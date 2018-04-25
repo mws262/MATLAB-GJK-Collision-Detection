ef506999660e4692c8ea8ef1062bc102f4122d31
Max Frei <36265931+maxfrei750@users.noreply.github.com>
2018-04-25 17:47:45 +0200

    Increase compatibility
    Getfield works with any kind of object (i.e. also patch-objects) and structs.

--- GJK.m
+++ GJK.m
@@ -191,9 +191,9 @@ end
 
 function point = getFarthestInDir(shape, v)
 %Find the furthest point in a given direction for a shape
-XData = get(shape,'XData'); % Making it more compatible with previous MATLAB releases.
-YData = get(shape,'YData');
-ZData = get(shape,'ZData');
+XData = getfield(shape,'XData'); % Making it more compatible with previous MATLAB releases.
+YData = getfield(shape,'YData');
+ZData = getfield(shape,'ZData');
 dotted = XData*v(1) + YData*v(2) + ZData*v(3);
 [maxInCol,rowIdxSet] = max(dotted);
 [maxInRow,colIdx] = max(maxInCol);
